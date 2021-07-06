3f6cc1c746a46165b53a40ac69ba114739b6e2c6
Kieran O'Sullivan <8798449+Kidsan@users.noreply.github.com>
2021-07-06 20:41:19 +0100

    add further route examples

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, Post, Header, Param } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  @Header('Cache-Control', 'none')
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(@Req() request: Request): string {
     console.log(request.baseUrl);
     return 'This action returns all cats';
+  }
+
+  @Get()
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
