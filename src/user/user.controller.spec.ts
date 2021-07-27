4ecacd0fd22d9412e2e89c1ba4a55cb8b9ed1964
Kieran O'Sullivan <8798449+Kidsan@users.noreply.github.com>
2021-07-27 17:41:10 +0100

    Create node.js.yml

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,33 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userService = moduleRef.get<UserService>(UserService);
+    userController = moduleRef.get<UserController>(UserController);
+    return;
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of user', async () => {
+      console.log(userController)
+      const result = [
+        { id: 1, firstName: 'kidsan', lastName: 'kidsan', isActive: true },
+      ];
+      jest
+        .spyOn(userService, 'findAll')
+        .mockImplementation(() => new Promise((res) => res(result)));
+
+      expect(await userController.findAll()).toBe(result);
+    });
   });
 });
