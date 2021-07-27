15d601838e1e371d745cd1fcb0e4afc8e2e5dda4
Kieran O'Sullivan <8798449+Kidsan@users.noreply.github.com>
2021-07-27 20:15:09 +0100

    test(typeorm): add missing typeorm deps

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,17 +1,33 @@
 import { Test } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
 describe('UserController', () => {
   let userController: UserController;
   let userService: UserService;
 
   beforeEach(async () => {
+    console.log('foo');
     const moduleRef = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'test',
+          entities: [User],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
-
+    console.log(moduleRef);
     userService = moduleRef.get<UserService>(UserService);
     userController = moduleRef.get<UserController>(UserController);
     return;
@@ -19,7 +35,7 @@ describe('UserController', () => {
 
   describe('findAll', () => {
     it('should return an array of user', async () => {
-      console.log(userController)
+      console.log(userController);
       const result = [
         { id: 1, firstName: 'kidsan', lastName: 'kidsan', isActive: true },
       ];
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
-describe('UserService', () => {
+describe.only('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'test',
+          entities: [User],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([User])
+      ],
       providers: [UserService],
     }).compile();
 
